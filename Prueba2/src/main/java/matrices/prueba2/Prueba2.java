cb50552c1176501b47b9e6814f46c08133d0b42e
Astivero-x3d <ismaelast2005@gmail.com>
2024-01-17 20:43:07 +0100

    Haciendo pruebas del comit

---
+++ Prueba2/src/main/java/matrices/prueba2/Prueba2.java
@@ -0,0 +1,12 @@
+package matrices.prueba2;
+
+/**
+ *
+ * @author Ismael_Ast
+ */
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Haciendo en comit");
+    }
+}
